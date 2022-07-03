9ee85461f24bc611e1b85c239665d951f089c36f
kunjal-parmar <67464846+kunjal-parmar@users.noreply.github.com>
2022-07-03 18:40:20 +0530

    Add files via upload

---
+++ build/precache-manifest.00e22f80a4ea3ee2c025222bc8f87275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dffd759e94c4da484444",
+    "url": "/static/css/main.1736b5ff.chunk.css"
+  },
+  {
+    "revision": "dffd759e94c4da484444",
+    "url": "/static/js/main.dffd759e.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "29b76d4c86356e09cc9b",
+    "url": "/static/js/2.29b76d4c.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "f1a54b9ce59296f1d7ee8b8e0de1be90",
+    "url": "/index.html"
+  }
+];
